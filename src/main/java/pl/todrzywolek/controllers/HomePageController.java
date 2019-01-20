b13214f3916ff09783cc0a157d960fbc4ede6875
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-01-20 11:49:01 +0100

    initial commit spring mvc basic web.xml configuration template

---
+++ src/main/java/pl/todrzywolek/controllers/HomePageController.java
@@ -0,0 +1,14 @@
+package pl.todrzywolek.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MVC Template - Home Page</title>
+</head>
+<body>
+    <h1>Spring MVC Demo - Home Page</h1>
+
+</body>
+</html>
